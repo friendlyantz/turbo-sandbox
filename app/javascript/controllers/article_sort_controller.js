d772a5a9b18af62c719bea85fe02d166ab1ccfa4
Anton Panteleev <friendlyantz@pm.me>
2023-05-25 14:46:55 +1000

    Add ordering DropDown front end

--- app/javascript/controllers/article_sort_controller.js
+++ app/javascript/controllers/article_sort_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="article-sort"
 export default class extends Controller {
+  static targets = [ "select" ]
   connect() {
     console.log("Hello, Stimulus!", this.element)
+    console.log(this.selectTarget.value)
   }
 }
