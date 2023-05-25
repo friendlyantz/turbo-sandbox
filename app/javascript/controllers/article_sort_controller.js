dcbe4a32341489d314e10d792cc323efcdaa6a3b
Anton Panteleev <friendlyantz@pm.me>
2023-05-25 15:09:17 +1000

    Implement Sort options Stimulus front end

--- app/javascript/controllers/article_sort_controller.js
+++ app/javascript/controllers/article_sort_controller.js
@@ -7,4 +7,17 @@ export default class extends Controller {
     console.log("Hello, Stimulus!", this.element)
     console.log(this.selectTarget.value)
   }
+
+  sortPosts() {
+    const sortValue = this.selectTarget.value;
+    const url = `/articles?sort_by=${sortValue}`; // not ideal, prone to route change
+
+    Turbo.visit(url);
+  }
+
+
+  sort() {
+    console.log("update");
+    this.sortPosts();
+  }
 }
